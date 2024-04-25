860ac6ff7710f4cae69385123264237a1f1d149e
Firdous <firdousrahaman42@gmail.com>
2024-04-25 10:25:10 +0530

    Add new dashboard page and update middleware for authentication

---
+++ src/app/(app)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>This page is only visible to signed in users</p>
+        </div>
+    )
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -9,20 +9,20 @@ export async function middleware(request: NextRequest) {
     console.log("Token: ", token);
     const url = request.nextUrl;
 
-    if (token &&
-        (
-            url.pathname.startsWith('/signin') ||
-            url.pathname.startsWith('/signup') ||
-            url.pathname.startsWith('/verify') ||
-            url.pathname.startsWith('/')
-        )
-    ) {
-        return NextResponse.redirect(new URL('/dashboard', request.url))
-    }
+    // if (token &&
+    //     (
+    //         url.pathname.startsWith('/signin') ||
+    //         url.pathname.startsWith('/signup') ||
+    //         url.pathname.startsWith('/verify') ||
+    //         url.pathname.startsWith('/')
+    //     )
+    // ) {
+    //     return NextResponse.redirect(new URL('/dashboard', request.url))
+    // }
 
-    if (!token && url.pathname.startsWith('/dashboard')) {
-        return NextResponse.redirect(new URL('/signin', request.url))
-    }
+    // if (!token && url.pathname.startsWith('/dashboard')) {
+    //     return NextResponse.redirect(new URL('/signin', request.url))
+    // }
 
     return NextResponse.next()
 }
